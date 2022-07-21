96c2a0ef51a4abcdde5e378590565cbccf4e4b1e
Eric RANDRETSARIMANANA <eric_ra@absys.fr>
2022-07-21 15:34:48 +0200

    effect bottom bar

--- src/pages/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    lol
-</template>
-
-<script>
-export default {
-    name: "About"
-}
-</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,61 +1,6 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
-  <nav>
-    <div
-      class="
-        flex
-        justify-between
-        items-center
-        border
-        px-5
-        py-2
-        rounded-full
-        mt-3
-        mx-4
-        shadow-lg
-      "
-    >
-      <div class="flex">
-        <i class="mr-5">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </i>
-        <p>Destination</p>
-      </div>
-
-      <i class="rounded-full border-solid border-2 px-1 py-1">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-          />
-        </svg>
-      </i>
-    </div>
-
-    <hr class="mt-4 hidden md:block" />
-    <div class="border-5"></div>
-  </nav>
+  <nav-menu />
 
   <!-- Menu section btn -->
   <div
@@ -67,7 +12,9 @@
       max-w-full
       overflow-x-scroll
       border-b-solid
+      md:border-none
       shadow-md
+      md:shadow-none
       scrollbar-nav
     "
   >
@@ -562,67 +509,72 @@
   </div>
 
   <!-- component -->
-  <div class="fixed bottom-0 bg-blue-400 w-full py-5">
-    <button class="py-2 mx-3">
-      <span class="flex flex-col items-center">
-        <svg
-          class="inline-block w-5 h-5 mr-1"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-          ></path>
-        </svg>
-        <div class="block relative whitespace-nowrap">
-          <span class="inline-block">Explorer</span>
-        </div>
-      </span>
-    </button>
-    <button class="py-2 mx-3">
-      <span class="flex flex-col items-center">
-        <svg
-          class="inline-block w-5 h-5 mr-1"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-          ></path>
-        </svg>
-        <div class="block relative whitespace-nowrap">
-          <span class="inline-block">Favoris</span>
-        </div>
-      </span>
-    </button>
-    <button class="py-2 mx-3">
-      <span class="flex flex-col items-center">
-        <svg
-          class="inline-block w-5 h-5 mr-1"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-          ></path>
-        </svg>
-        <div class="block relative whitespace-nowrap">
-          <span class="inline-block">Connexion</span>
-        </div>
-      </span>
-    </button>
+  <div ref="footbar" class="test fixed bottom-0 bg-white w-full py-2">
+    <div class="text-center">
+      <button class="py-2 mx-3 hover:transition">
+        <span class="flex flex-col items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline-block h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+          <div class="block relative whitespace-nowrap">
+            <span class="inline-block text-xs">Explorer</span>
+          </div>
+        </span>
+      </button>
+      <button class="py-2 mx-3">
+        <span class="flex flex-col items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline-block h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            />
+          </svg>
+          <div class="block relative whitespace-nowrap">
+            <span class="inline-block text-xs">Favoris</span>
+          </div>
+        </span>
+      </button>
+      <button class="py-2 mx-3">
+        <span class="flex flex-col items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline-block h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <div class="block relative whitespace-nowrap">
+            <span class="inline-block text-xs">Connexion</span>
+          </div>
+        </span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -633,10 +585,51 @@
 </style>
 
 <script>
+import NavMenu from "@/components/NavMenu";
+
 export default {
   name: "HelloWorld",
+  components: {
+    NavMenu,
+  },
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      showNavbar: true,
+      lastScrollPosition: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      const currentScrollPosition =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentScrollPosition < 0) {
+        return;
+      }
+
+      if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 60) {
+        return;
+      }
+
+      this.showNavbar = currentScrollPosition < this.lastScrollPosition;
+      if (!this.showNavbar) {
+        this.$refs.footbar.classList.add(
+          "transition-opacity", "duration-700", "ease-out", "opacity-0"
+        );
+      }else{
+        this.$refs.footbar.classList.remove("transition-opacity", "duration-300", "ease-out", "opacity-0")
+      }
+      this.lastScrollPosition = currentScrollPosition;
+    },
   },
 };
 </script>
